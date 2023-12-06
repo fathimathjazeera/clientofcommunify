2997f40519135d2529102fccdaaf82c1334aeec7
Ashfaq Haneef <78129119+mrthangu@users.noreply.github.com>
2023-12-06 08:38:12 +0530

    updation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,21 +12,22 @@ function Home() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
   const [postId, setPostId] = useState();
+  const [userId,setUserId] = useState()
   const { isLoggedIn,setUsername } = useContext(MyContext);
   const navigation = useNavigate();
+  console.log(popularData,"from popular");
 
 
 
 
-
-  const fetchPosts = async (id) => {
+  const fetchPosts = async () => {
     try {
       const response = await axios.get(
         "https://communify-server.mrzera.xyz/api/viewposts"
       );
       const filteredPost = response.data.data.filter((post) => {
-        console.log(post,"post id")
-        return id != post.postedBy._id;
+
+        return userId != post.postedBy._id;
       });
       setFiltered(filteredPost);
     } catch (error) {
@@ -52,13 +53,12 @@ function Home() {
       const { status, message, data } = response.data;
       if (status === "success") {
         setPopularData(data);
-
-        console.log("successfully fetched popular", data);
       }
     } catch (err) {
       console.log(err.message);
     }
   };
+
 
   const onSelectFilter = async (tabName) => {
     setActiveTab(tabName);
@@ -68,6 +68,7 @@ function Home() {
       await fetchPosts();
     }
   };
+
 
 
   const openModal = (postId) => {
@@ -150,7 +151,7 @@ function Home() {
       try {
         const decodedToken = JSON.parse(atob(authToken.split(".")[1]));
         const id = decodedToken.id;
-        fetchPosts(id);
+       setUserId(id)
         setUsername(decodedToken.username);
       } catch (error) {
         console.error("Error decoding token:", error);
@@ -372,9 +373,185 @@ function Home() {
         </>
       )}
 
+{activeTab === "new" && (
+        <>
+          {filtered.map((post) => (
+            <div
+              key={post._id}
+              className="post-card"
+              style={{
+                height: post.image ? "647px" : "200px",
+              }}
+            >
+              <div className="upvoteClass">
+                <svg
+                  onClick={() => {
+                    upvote(post._id);
+                  }}
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="m4 14h2 2v3 4c0 .553.447 1 1 1h6c.553 0 1-.447 1-1v-5-2h1 3c.385 0 .734-.221.901-.566.166-.347.12-.758-.12-1.059l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10c-.24.301-.286.712-.12 1.059.167.345.516.566.901.566z" />
+                </svg>
+                <p>{post?.totalvote}</p>
+                <svg
+                  onClick={() => {
+                    downvote(post._id);
+                  }}
+                  style={{ transform: "scale(1,-1)" }}
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="m4 14h2 2v3 4c0 .553.447 1 1 1h6c.553 0 1-.447 1-1v-5-2h1 3c.385 0 .734-.221.901-.566.166-.347.12-.758-.12-1.059l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10c-.24.301-.286.712-.12 1.059.167.345.516.566.901.566z" />
+                </svg>
+              </div>
+              <h3
+                onClick={() => {
+                  navigation(`/r/${post.subreddit}`);
+                }}
+              >
+                r/{post.subreddit}
+              </h3>
+              <h3
+                style={{ marginLeft: "100px" }}
+                onClick={() => {
+                  navigation(`/${post._id}`);
+                }}
+              >
+                {post.title}
+              </h3>
+              {post.image ? (
+                <img
+                  onClick={() => {
+                    navigation(`/${post._id}`);
+                  }}
+                  src={post.image}
+                  alt=""
+                  style={{
+                    width: "500px",
+                    height: "500px",
+                    marginLeft: "90px",
+                    borderRadius: "5px",
+                  }}
+                />
+              ) : null}
+              <p
+                style={{ marginLeft: "100px" }}
+                onClick={() => {
+                  navigation(`/${post._id}`);
+                }}
+              >
+                {post.content}
+              </p>
+              <div className="button-container">
+                <button className="button comment">Comment</button>
+                <button
+                  className="button share"
+                  onClick={() => openModal(post._id)}
+                >
+                  Share
+                </button>
+                <button className="button report">Report</button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
 
 
 
+{activeTab === "top" && (
+        <>
+          {filtered.map((post) => (
+            <div
+              key={post._id}
+              className="post-card"
+              style={{
+                height: post.image ? "647px" : "200px",
+              }}
+            >
+              <div className="upvoteClass">
+                <svg
+                  onClick={() => {
+                    upvote(post._id);
+                  }}
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="m4 14h2 2v3 4c0 .553.447 1 1 1h6c.553 0 1-.447 1-1v-5-2h1 3c.385 0 .734-.221.901-.566.166-.347.12-.758-.12-1.059l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10c-.24.301-.286.712-.12 1.059.167.345.516.566.901.566z" />
+                </svg>
+                <p>{post?.totalvote}</p>
+                <svg
+                  onClick={() => {
+                    downvote(post._id);
+                  }}
+                  style={{ transform: "scale(1,-1)" }}
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="m4 14h2 2v3 4c0 .553.447 1 1 1h6c.553 0 1-.447 1-1v-5-2h1 3c.385 0 .734-.221.901-.566.166-.347.12-.758-.12-1.059l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10c-.24.301-.286.712-.12 1.059.167.345.516.566.901.566z" />
+                </svg>
+              </div>
+              <h3
+                onClick={() => {
+                  navigation(`/r/${post.subreddit}`);
+                }}
+              >
+                r/{post.subreddit}
+              </h3>
+              <h3
+                style={{ marginLeft: "100px" }}
+                onClick={() => {
+                  navigation(`/${post._id}`);
+                }}
+              >
+                {post.title}
+              </h3>
+              {post.image ? (
+                <img
+                  onClick={() => {
+                    navigation(`/${post._id}`);
+                  }}
+                  src={post.image}
+                  alt=""
+                  style={{
+                    width: "500px",
+                    height: "500px",
+                    marginLeft: "90px",
+                    borderRadius: "5px",
+                  }}
+                />
+              ) : null}
+              <p
+                style={{ marginLeft: "100px" }}
+                onClick={() => {
+                  navigation(`/${post._id}`);
+                }}
+              >
+                {post.content}
+              </p>
+              <div className="button-container">
+                <button className="button comment">Comment</button>
+                <button
+                  className="button share"
+                  onClick={() => openModal(post._id)}
+                >
+                  Share
+                </button>
+                <button className="button report">Report</button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
 
 
 
