f2bf88229f6878caceabbb06b05a7d23f6118927
jazeera <113518710+fathimathjazeera@users.noreply.github.com>
2023-12-11 11:11:22 +0530

    Update Profile.jsx

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -40,7 +40,7 @@ const handleEditInputChange = (e) => {
 
 const handleProfileEditSubmit = async() => {
   const token= localStorage.getItem('authToken')
-await axios.put('https://communify-server.mrzera.xyz/api/users/editprofile',{
+await axios.put('https://communify-server.mrzera.in/api/users/editprofile',{
 user
 },{
 headers:{
@@ -63,7 +63,7 @@ const navigation=useNavigate()
   const deleteProfile=async()=>{
     try{
       const token=localStorage.getItem('authToken')
-   const response=   await axios.delete('https://communify-server.mrzera.xyz/api/users/deleteprofile',{
+   const response=   await axios.delete('https://communify-server.mrzera.in/api/users/deleteprofile',{
        headers:{
          Authorization:`Bearer ${token}`
        }
@@ -148,7 +148,7 @@ const navigation=useNavigate()
   const userUpvoted = async () => {
     const token = localStorage.getItem('authToken');
     try {
-      const response = await axios.get('https://communify-server.mrzera.xyz/api/users/userupvote', {
+      const response = await axios.get('https://communify-server.mrzera.in/api/users/userupvote', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -167,7 +167,7 @@ const navigation=useNavigate()
   const userDownvoted = async () => {
     const token = localStorage.getItem('authToken');
     try {
-      const response = await axios.get('https://communify-server.mrzera.xyz/api/users/userdownvote', {
+      const response = await axios.get('https://communify-server.mrzera.in/api/users/userdownvote', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -188,7 +188,7 @@ const navigation=useNavigate()
     try{
     const token=localStorage.getItem('authToken')
     
-    await axios.delete(`https://communify-server.mrzera.xyz/api/users/deletepost/${id}`,{
+    await axios.delete(`https://communify-server.mrzera.in/api/users/deletepost/${id}`,{
       headers:{
         Authorization:`Bearer ${token}`
       }
