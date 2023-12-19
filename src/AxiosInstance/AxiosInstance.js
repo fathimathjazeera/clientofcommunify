03acbe5cc0fd724bec70a639620ff1a2ce5ecb20
fathimathjazeera <jezijazeera@gmail.com>
2023-12-19 18:14:45 +0530

    final commit

--- src/AxiosInstance/AxiosInstance.js
+++ src/AxiosInstance/AxiosInstance.js
@@ -1,23 +1,26 @@
 import axios from "axios";
 
+
 const axiosInstance = axios.create({
-  baseURL: "https://communify-server.mrzera.in",
+  baseURL: "https://communify-server.mrzera.in", 
 });
 
 axiosInstance.interceptors.request.use((config) => {
   const jwtToken = localStorage.getItem("authToken");
   const adminAuthToken = localStorage.getItem("adminAuthToken");
 
+  if (jwtToken !== "" && jwtToken !== "null") {
+    // Additional check for token validity if needed
+
+    // Set the Authorization header
+    config.headers.Authorization = `Bearer ${jwtToken}`;
+  }
+
   if (adminAuthToken !== "" && adminAuthToken !== "null") {
     // Additional check for admin token validity if needed
 
-    // Set the Authorization header for admin token
+    // Set the Authorization header
     config.headers.Authorization = `Bearer ${adminAuthToken}`;
-  } else if (jwtToken !== "" && jwtToken !== "null") {
-    // Additional check for user token validity if needed
-
-    // Set the Authorization header for user token
-    config.headers.Authorization = `Bearer ${jwtToken}`;
   }
 
   return config;
