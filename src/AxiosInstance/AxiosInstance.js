79055459921c4ab4dcff0d5222795765d896d9b1
fathimathjazeera <jezijazeera@gmail.com>
2023-12-19 18:25:27 +0530

    committed finally

--- src/AxiosInstance/AxiosInstance.js
+++ src/AxiosInstance/AxiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
-  baseURL: "https://communify-server.mrzera.in", 
+  baseURL: "https://communify-server.mrzera.in",
 });
 
 axiosInstance.interceptors.request.use((config) => {
@@ -12,14 +11,12 @@ axiosInstance.interceptors.request.use((config) => {
   if (jwtToken !== "" && jwtToken !== "null") {
     // Additional check for token validity if needed
 
-    // Set the Authorization header
+    // Set the Authorization header for user token
     config.headers.Authorization = `Bearer ${jwtToken}`;
-  }
-
-  if (adminAuthToken !== "" && adminAuthToken !== "null") {
+  } else if (adminAuthToken !== "" && adminAuthToken !== "null") {
     // Additional check for admin token validity if needed
 
-    // Set the Authorization header
+    // Set the Authorization header for admin token
     config.headers.Authorization = `Bearer ${adminAuthToken}`;
   }
 
